5a6cfd695fbe776c3aa9df7a3d9e1287efb9793a
Roberto Okamura <rob.okamura@gmail.com>
2022-07-11 21:40:43 -0300

    other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
-
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>OLA MUNDO</h1>  
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
-  )
-  
+  );
 }
 
-export default App
+export default App;
