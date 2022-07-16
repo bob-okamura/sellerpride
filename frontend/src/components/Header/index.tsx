ad21937bc3f803eda7fb664e7712730c4b44c182
Roberto Okamura <rob.okamura@gmail.com>
2022-07-16 02:05:28 -0300

    Update Header and seed

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
             <img src={logo} alt="DSMeta"/>
             <h1>DSMeta</h1>
             <p>Desenvolvido por 
-                <a href="https://www.instagram.com/bob.okamura/"> @bob.okamura</a>
+                <a href="https://github.com/bob-okamura/dsmeta"> @bob.okamura</a>
             </p>
         </div>    
     </header>  
